6e67247eb99d9d5cc5327e829faaa033a645713a
Ryan Millard <142347829+Ryan-Millard@users.noreply.github.com>
2025-07-08 23:12:19 +0200

    update(UI): Set up simple UI & themes (#3)
    * add(Vite alias): Alias wasm build dir - simpler imports
    
    * refactor(Modularise Code): Move App.jsx logic to own file and use it in Home.jsx
    
    * update(LCIENSE): Comply with reactbits.dev
    
    * update(UI): Set up simple UI & themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
-import { loadImageToUint8Array } from './utils/image-utils.js';
-import createImageModule from './wasm/build/image_utils.js';
+import { useEffect } from 'react';
+import Home from '@pages/Home';
 
 function App() {
-	const [mod, setMod] = useState(null);
-	const canvasRef = useRef(null);
-
 	useEffect(() => {
-		createImageModule().then(setMod);
-	}, []);
+		document.documentElement.classList.add('dark');
+	}, []); // empty dependency array = run once after mount
 
-	const handleFile = async (e) => {
-		const file = e.target.files[0];
-		if (!file || !mod) return;
-
-		const { pixels, width, height } = await loadImageToUint8Array(file);
-		const size = pixels.length;
-		console.log(pixels);
-
-		// Allocate memory in WASM
-		const ptr = mod._malloc(size);
-		mod.HEAPU8.set(pixels, ptr); // Copy data into WASM memory
-
-		// Call C++ function
-		mod._invert_image(ptr, size);
-
-		// Copy back modified data
-		const modified = new Uint8ClampedArray(mod.HEAPU8.subarray(ptr, ptr + size));
-
-		// Free WASM memory
-		mod._free(ptr);
-
-		// Do something with the modified image (e.g. draw to canvas)
-		const canvas = document.getElementById('out');
-		const ctx = canvas.getContext('2d');
-		const imageData = new ImageData(modified, width, height);
-		canvas.width = width;
-		canvas.height = height;
-		ctx.putImageData(imageData, 0, 0);
-	};
-
-	return (
-		<>
-			<input type="file" accept="image/*" onChange={handleFile} />
-			<canvas ref={canvasRef} id="out" />
-		</>
-	);
+	return <Home />;
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,13 @@ export default defineConfig({
 	],
 	// Ensure Vite copies .wasm files
 	assetsInclude: ['**/*.wasm'],
+	resolve: {
+		alias: {
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
+			'@wasm': path.resolve(__dirname, 'src/wasm/build'),
+		}
+	}
 })
