dbfbe2be0c15b2c4d5170630c13ea404bf54be0d
Ryan-Millard <millardryandevon@gmail.com>
2025-07-23 22:03:40 +0200

    fix(Variable Naming): black_threshold_image now uses the same paramer names in .h & .cpp files

--- src/wasm/modules/image/src/image_utils.cpp
+++ src/wasm/modules/image/src/image_utils.cpp
@@ -50,7 +50,7 @@ extern "C" {
 			std::memcpy(ptr, modified.data(), modified.size() * sizeof(ImageLib::RGBAPixel<uint8_t>));
 		}
 
-		void black_threshold_image(uint8_t* ptr, const int width, const int height, const int threshold) {
+		void black_threshold_image(uint8_t* ptr, const int width, const int height, const int num_thresholds) {
 			ImageLib::Image<ImageLib::RGBAPixel<uint8_t>> img;
 			img.loadFromBuffer(ptr, width, height, ImageLib::RGBA_CONVERTER<uint8_t>);
 
