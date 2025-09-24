ac38ee7e60afdd24e4f51a865021bfd96e4dde04
Ryan-Millard <millardryandevon@gmail.com>
2025-09-24 21:18:15 +0200

    create(uint8ClampedArrayToSVG): Display nice SVG on screen

--- src/utils/image-utils.js
+++ src/utils/image-utils.js
@@ -1,3 +1,5 @@
+import ImageTracer from 'imagetracerjs';
+
 export function loadImageToUint8Array(file) {
 	return new Promise((resolve) => {
 		const img = new Image();
@@ -15,3 +17,30 @@ export function loadImageToUint8Array(file) {
 		img.src = URL.createObjectURL(file);
 	});
 }
+
+export const uint8ClampedArrayToSVG = async ({ pixels, width, height }) => {
+	// Force full alpha
+	for (let i = 3; i < pixels.length; i += 4) pixels[i] = 255;
+
+	// Create a canvas
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	const ctx = canvas.getContext('2d');
+
+	try {
+		const imageData = new ImageData(pixels, width, height);
+		ctx.putImageData(imageData, 0, 0);
+
+		const svgString = ImageTracer.imagedataToSVG(imageData, {
+			ltres: 0,
+			rightangleenhance: false,
+		});
+
+		return svgString;
+
+	} catch (error) {
+		console.error('Error in SVG conversion:', error);
+		return null;
+	}
+};
