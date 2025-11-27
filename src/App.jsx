9483779b59e54994aacc1e69a61e7a0a46e0619a
Ryan-Millard <millardryandevon@gmail.com>
2025-11-27 21:27:03 +0200

    creat(About Page): Basic page with hero, what its about, motivation, tech stack, CTA, about author

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,21 @@ import Editor from '@pages/Editor';
 import Loading from '@pages/Loading';
 
 const Credits = lazy(() => import('@pages/Credits'));
+const About = lazy(() => import('@pages/About'));
 
 export default function App() {
   return (
     <>
       <NavBar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/editor" element={<Editor />} />
-        <Route
-          path="/credits"
-          element={
-            <Suspense fallback={<Loading />}>
-              <Credits />
-            </Suspense>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
---
+++ src/pages/About/CTA.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import GlassCard from '@components/GlassCard';
+import styles from './About.module.css';
+
+const CTA = () => (
+	<GlassCard className={styles.container}>
+		<h2>Try Img2Num!</h2>
+		<p className="flex-center">Ready to turn your favourite photos into colour-by-number masterpieces?</p>
+		<Link to="/">Launch Img2Num</Link>
+	</GlassCard>
+);
+
+export default CTA;
