853f67b9749b529aa0f109bf75e5e1fc87266b1b
Ryan-Millard <millardryandevon@gmail.com>
2025-12-05 17:41:54 +0200

    create(Docusaurus Skeleton): Basic template with starter code

---
+++ docs/plugins/webpack-alias/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+
+module.exports = function (context, options) {
+  return {
+    name: 'webpack-alias-plugin',
+    configureWebpack() {
+      return {
+        resolve: {
+          alias: {
+            // Allow access of code inside main app
+            '@img2num': path.resolve(__dirname, '..', 'src'),
+          },
+        },
+        module: {
+          rules: [
+            {
+              // Ensure babel-loader transpiles JS/JSX inside src
+              test: /\.m?jsx?$/,
+              include: [path.resolve(__dirname, '..', 'src')],
+              use: {
+                loader: require.resolve('babel-loader'),
+                options: {
+                  // Docusaurus' Babel default config
+                  presets: [],
+                },
+              },
+            },
+          ],
+        },
+      };
+    },
+  };
+};
