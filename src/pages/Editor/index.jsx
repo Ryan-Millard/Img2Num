b1e6046f88e81407313e77748b6c858f85167a1f
Ryan-Millard <millardryandevon@gmail.com>
2025-12-14 22:16:35 +0200

    feat(Print Image): Add print utilities & hook up to UI

--- src/pages/Editor/index.jsx
+++ src/pages/Editor/index.jsx
@@ -1,11 +1,16 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Printer } from "lucide-react";
+
 import GlassCard from '@components/GlassCard';
 import styles from './Editor.module.css';
 import EditorHelmet from './EditorHelmet';
+import { printSVG } from '@utils/image-utils';
+import Modal from '@components/Modal';
+import Switch from '@components/Switch';
 
 export default function Editor() {
   const { state } = useLocation();
-  console.log(state);
   const { svg } = state || {};
 
   if (!svg) {
@@ -17,13 +22,33 @@ export default function Editor() {
     );
   }
 
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const toggleSwitch = () => setIsSwitchOn(prev => !prev);
+
   return (
     <>
       <EditorHelmet />
 
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Print Image">
+        <div className="flex-center flex-column">
+          <div
+            className={isSwitchOn ? styles.emptyPathSvgContainer : undefined}
+            id={styles.noEditSvgContainer}
+            dangerouslySetInnerHTML={{ __html: svg }}
+          />
+          <span>{isSwitchOn ? "Line Art" : "Normal"}</span>
+          <Switch isOn={isSwitchOn} onToggle={toggleSwitch} />
+          <button onClick={() => printSVG(svg, { lineArt: isSwitchOn })}>Print Image</button>
+        </div>
+      </Modal>
+
+      <button onClick={() => setModalOpen(true)}><Printer size={24} /> Print</button>
+
       <GlassCard>
         <div
-          className={`flex-center ${styles.svgContainer}`}
+          className={`flex-center ${styles.svgContainer} ${styles.emptyPathSvgContainer}`}
           dangerouslySetInnerHTML={{ __html: svg }}
           onClick={(e) => {
             if (e.target.tagName === 'path') {
