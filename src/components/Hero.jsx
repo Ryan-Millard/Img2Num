78e0d16b4306417db92f502dd0210212a3ead5e2
Utkarsha Priyanshi Xalxo <ashixalxo80@gmail.com>
2025-12-14 18:51:27 +0530

    Refactor Hero layout using CSS modules

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import styles from './Hero.module.css';
 import GlassCard from '@components/GlassCard';
 
 const Hero = ({ header, description }) => (
-  <GlassCard className="text-center">
+  <GlassCard className={styles.heroContainer}>
     <h1>{header}</h1>
     <p className={styles.heroParagraph}>{description}</p>
   </GlassCard>
