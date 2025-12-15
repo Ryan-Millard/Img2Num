6a953ab3effdad72276fcefacd71176511e4c7a1
Ryan Millard <142347829+Ryan-Millard@users.noreply.github.com>
2025-12-15 16:44:00 +0200

    Update src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import styles from './Hero.module.css';
 import GlassCard from '@components/GlassCard';
 
 const Hero = ({ header, description }) => (
-  <GlassCard className={styles.heroContainer}>
+  <GlassCard className="text-center">
     <h1>{header}</h1>
     <p className={styles.heroParagraph}>{description}</p>
   </GlassCard>
