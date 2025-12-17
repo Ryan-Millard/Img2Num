36645466ac11504377cf02d19067686331036037
Ryan-Millard <millardryandevon@gmail.com>
2025-12-17 16:04:35 +0200

    refactor(docs help.js): Integrate with new help functions for uniformity

--- docs/scripts/help.js
+++ docs/scripts/help.js
@@ -1,28 +1,15 @@
-#!/usr/bin/env node
-const fs = require('fs');
+import { runFuzzyCli } from "../../scripts/lib/cli-fuzzy.js";
+import { readPackageJsonScripts } from "../../scripts/lib/read-packageJson-scripts.js";
 
-// Read package.json dynamically
-const pkg = JSON.parse(fs.readFileSync(require('path').resolve(__dirname, '../package.json'), 'utf-8'));
+const title =
+`Img2Num Docs CLI Scripts
+Also see: https://ryan-millard.github.io/Img2Num/info/docs/category/-project-scripts
+`;
 
-// Optional descriptions for scripts
-const descriptions = {
-  docusaurus: 'Run Docusaurus CLI',
-  start: 'Start the Docusaurus dev server',
-  build: 'Build the Docusaurus site',
-  swizzle: 'Customize Docusaurus theme components',
-  deploy: 'Deploy the site',
-  clear: 'Clear Docusaurus cache',
-  serve: 'Serve the production build locally',
-  'write-translations': 'Write translation files',
-  'write-heading-ids': 'Add heading IDs for MDX',
-};
+const { flat: items, basicItems } = readPackageJsonScripts(new URL("../package.json", import.meta.url));
 
-// Print scripts
-console.log('Available docs scripts:');
-for (const [name, command] of Object.entries(pkg.scripts)) {
-  const desc = descriptions[name] || 'No description';
-  console.log(`\n- ${name}: ${desc}`);
-  console.log(`    > ${command}`);
-}
-
-console.log('\nRun a script with `npm run <script-name>`');
+runFuzzyCli({
+  title,
+  items,
+  basicItems,
+});
