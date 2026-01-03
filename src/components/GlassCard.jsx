36b8c0f42ca0bdda9b8ddc530d098f39048963aa
Sri Ram Charan Nandigam <sriramcharannandigam@gmail.com>
2026-01-03 11:16:21 +0530

    docs: add propTypes to GlassCard component

--- src/components/GlassCard.jsx
+++ src/components/GlassCard.jsx
@@ -1,10 +1,34 @@
+import PropTypes from 'prop-types';
 import styles from './GlassCard.module.css';
 
 // eslint-disable-next-line no-unused-vars
 const GlassCard = ({ as: Tag = 'div', children, ...rest }) => (
-  <Tag {...rest} className={`text-center glass ${styles.card} ${rest.className || ''}`}>
+  <Tag
+    {...rest}
+    className={`text-center glass ${styles.card} ${rest.className || ''}`}
+  >
     {children}
   </Tag>
 );
+
+GlassCard.propTypes = {
+  /** HTML element to render (polymorphic support) */
+  as: PropTypes.oneOf([
+    'div',
+    'section',
+    'article',
+    'nav',
+    'main',
+    'aside',
+    'header',
+    'footer',
+  ]),
+
+  /** Content inside the GlassCard */
+  children: PropTypes.node,
+
+  /** Additional CSS classes */
+  className: PropTypes.string,
+};
 
 export default GlassCard;
