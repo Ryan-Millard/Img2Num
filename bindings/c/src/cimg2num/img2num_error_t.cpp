dc40187c4c5302811ae2e4da3e3dc26de33e97fa
Ryan-Millard <millardryandevon@gmail.com>
2026-02-22 16:37:21 +0200

    refactor(img2num_error_t): add static asserts

--- bindings/c/src/cimg2num/img2num_error_t.cpp
+++ bindings/c/src/cimg2num/img2num_error_t.cpp
@@ -3,11 +3,17 @@
 
 #include <cstring>
 
-extern "C" {
 static inline img2num_error_t cpp_error_to_c_error(img2num::Error cpp_error) {
+    static_assert(static_cast<int>(img2num::Error::OK)               == IMG2NUM_OK,                        "enum mismatch");
+    static_assert(static_cast<int>(img2num::Error::BAD_ALLOC)        == IMG2NUM_ERROR_BAD_ALLOC,           "enum mismatch");
+    static_assert(static_cast<int>(img2num::Error::INVALID_ARGUMENT) == IMG2NUM_ERROR_INVALID_ARGUMENT,    "enum mismatch");
+    static_assert(static_cast<int>(img2num::Error::RUNTIME)          == IMG2NUM_ERROR_RUNTIME,             "enum mismatch");
+    static_assert(static_cast<int>(img2num::Error::UNKNOWN)          == IMG2NUM_ERROR_UNKNOWN,             "enum mismatch");
+
     return static_cast<img2num_error_t>(cpp_error);
 }
 
+extern "C" {
 img2num_error_t img2num_get_last_error() {
     const img2num::Error cpp_err {img2num::get_last_error()};
     return cpp_error_to_c_error(cpp_err);
