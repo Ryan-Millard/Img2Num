77cffb67e1b79a157d6d2ab08402e3565d9e4e6e
Krasner <aakrasner@gmail.com>
2026-04-06 14:56:29 +0000

    Changes to webgpu to make it more stable in docker and wasm

--- core/tools/embed_shaders.py
+++ core/tools/embed_shaders.py
@@ -6,24 +6,28 @@ out = pathlib.Path(sys.argv[2])
 files = list(shader_dir.glob("*.wgsl"))
 
 with open(out, "w") as f:
-    f.write("#include <unordered_map>\n")
+    f.write("#include <array>\n")
     f.write("#include <string_view>\n")
     f.write("#include <string>\n\n")
 
+
     f.write("namespace embedded_shaders {\n")
+    f.write("struct ShaderEntry {\n")
+    f.write("    std::string_view id;\n")
+    f.write("    std::string_view source;\n")
+    f.write("};\n")
 
     for file in files:
         name = file.stem
         text = file.read_text()
 
         # escape for C++ raw string
-        f.write(f'static const char {name}[] = R"WGSL(\n{text}\n)WGSL";\n')
-
-    f.write("\ninline std::unordered_map<std::string,std::string_view> shaders = {\n")
-
+        f.write(f'constexpr char {name}[] = R"WGSL(\n{text}\n)WGSL";\n')
+    
+    f.write("constexpr std::array<ShaderEntry, 9> shaders = {{\n")
     for file in files:
         name = file.stem
         f.write(f'    {{"{name}", {name}}},\n')
+    f.write("}};\n")
 
-    f.write("};\n")
     f.write("}\n")
