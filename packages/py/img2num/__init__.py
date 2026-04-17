b80e436a6d8d04f5b0bff63e77b83e54d93b8b3e
Ryan-Millard <millardryandevon@gmail.com>
2026-04-17 02:14:35 +0200

    refactor(python-package): remove forgotten debug print

--- packages/py/img2num/__init__.py
+++ packages/py/img2num/__init__.py
@@ -1,7 +1,1 @@
 from .api import *
-from .api import gaussian_blur_fft as _gaussian_blur_fft
-
-
-def gaussian_blur_fft(*args, **kwargs):
-    print("Running gaussian blur")
-    return _gaussian_blur_fft(*args, **kwargs)
