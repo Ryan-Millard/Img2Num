dcceba03b151d7c31aed18b58abf95058d330891
Krasner <aakrasner@gmail.com>
2026-05-05 13:10:38 +0000

    add python typing hints

--- packages/py/img2num/api.py
+++ packages/py/img2num/api.py
@@ -1,4 +1,6 @@
+from typing import Tuple
 import numpy as np
+import numpy.typing as npt
 import inspect
 import functools
 from ._img2num import (
@@ -35,34 +37,34 @@ def _inject_dims(image_arg="image"):
 
 
 @_inject_dims("image")
-def gaussian_blur_fft(image, sigma, *, width, height):
+def gaussian_blur_fft(image: npt.NDArray[np.uint8], sigma: float, *, width: int, height: int) -> npt.NDArray[np.uint8]:
     return _gaussian_blur_fft(image, width, height, sigma)
 
 @_inject_dims("image")
-def invert_image(image, *, width, height):
+def invert_image(image: npt.NDArray[np.uint8], *, width: int, height: int) -> npt.NDArray[np.uint8]:
     return _invert_image(image, width, height)
 
 @_inject_dims("image")
-def threshold_image(image, num_thresholds, *, width, height):
+def threshold_image(image: npt.NDArray[np.uint8], num_thresholds: int, *, width: int, height: int) -> npt.NDArray[np.uint8]:
     return _threshold_image(image, width, height, num_thresholds)
 
 @_inject_dims("image")
-def black_threshold_image(image, num_thresholds, *, width, height):
+def black_threshold_image(image: npt.NDArray[np.uint8], num_thresholds: int, *, width: int, height: int) -> npt.NDArray[np.uint8]:
     return _black_threshold_image(image, width, height, num_thresholds)
 
 @_inject_dims("image")
-def bilateral_filter(image, sigma_spatial, sigma_range, color_space, *, width, height):
+def bilateral_filter(image: npt.NDArray[np.uint8], sigma_spatial: float, sigma_range: float, color_space: int, *, width: int, height: int) -> npt.NDArray[np.uint8]:
     return _bilateral_filter(image, width, height, sigma_spatial, sigma_range, color_space)
 
 @_inject_dims("data")
-def kmeans(data, k, max_iter, color_space, *, width, height):
+def kmeans(data: npt.NDArray[np.uint8], k: int, max_iter: int, color_space: int, *, width: int, height: int) -> Tuple[npt.NDArray[np.uint8], npt.NDArray[int]]:
     return _kmeans(data, width, height, k, max_iter, color_space)
 
 @_inject_dims("data")
-def labels_to_svg(data, labels, min_area, *, width, height):
+def labels_to_svg(data: npt.NDArray[np.uint8], labels: npt.NDArray[int], min_area: int, *, width: int, height: int) -> str:
     return _labels_to_svg(data, labels, width, height, min_area)
 
 @_inject_dims("image")
-def image_to_svg(image, *, width, height, config=None):
+def image_to_svg(image: npt.NDArray[np.uint8], *, width: int, height: int, config=None) -> str:
     # if config=None use default configs
     return _image_to_svg(image, width, height, config)
