31eb2d17811be70c092fdba8743eb9888c4f69c4
laxita <87566518+laxitajain@users.noreply.github.com>
2026-06-20 14:10:26 +0400

    fix(core): add guard against invalid num_thresholds, preventing division by zero (#445)
    Previously, there was a divide by zero (or lower) risk if the user supplied a value that was not a positive integer. This fixes that by returning early if bad arguments are supplied.
    
    This affects the `threshold_image` and `black_threshold_image` functions in the C++ library (`core/`).

--- core/src/internal/image_utils.cpp
+++ core/src/internal/image_utils.cpp
@@ -117,6 +117,9 @@ void invert_image(uint8_t* ptr, int width, int height) {
 }
 
 void threshold_image(uint8_t* ptr, const int width, const int height, const int num_thresholds) {
+    if (num_thresholds <= 0) {
+        return;
+    }
     const uint8_t REGION_SIZE(255 / num_thresholds); // Size of buckets per colour
 
     ImageLib::Image<ImageLib::RGBAPixel<uint8_t>> img;
@@ -136,6 +139,9 @@ void threshold_image(uint8_t* ptr, const int width, const int height, const int 
 void black_threshold_image(
     uint8_t* ptr, const int width, const int height, const int num_thresholds
 ) {
+    if (num_thresholds <= 0) {
+        return;
+    }
     ImageLib::Image<ImageLib::RGBAPixel<uint8_t>> img;
     img.loadFromBuffer(ptr, width, height, ImageLib::RGBA_CONVERTER<uint8_t>);
 
