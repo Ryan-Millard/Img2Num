5ebd02467493d136c6cfb263ae535396fd416f1c
Ryan Millard <142347829+Ryan-Millard@users.noreply.github.com>
2026-07-08 23:30:00 +0200

    docs: improve table of contents (#504)
    - Swizzle and improve styles
    - Add docusaurus-plugin-copy-page-button
    - Add "report issue" option

---
+++ docs/src/theme/DocItem/Paginator/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
+import DocPaginator from "@theme/DocPaginator";
+import styles from "./index.module.css";
+
+/**
+ * This extra component is needed, because <DocPaginator> should remain generic.
+ * DocPaginator is used in non-docs contexts too: generated-index pages...
+ */
+export default function DocItemPaginator() {
+  const { metadata } = useDoc();
+  return <DocPaginator className={styles.docPaginator} previous={metadata.previous} next={metadata.next} />;
+}
---
+++ docs/src/theme/EditThisPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Translate from "@docusaurus/Translate";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import Link from "@docusaurus/Link";
+import IconEdit from "@theme/Icon/Edit";
+export default function EditThisPage({ editUrl }) {
+  return (
+    <Link to={editUrl} className={ThemeClassNames.common.editThisPage} style={{ display: "flex", alignItens: "center" }}>
+      <IconEdit />
+      <Translate id="theme.common.editThisPage" description="The link label to edit the current page">
+        Edit
+      </Translate>
+    </Link>
+  );
+}
