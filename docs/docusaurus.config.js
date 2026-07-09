a523cfaa17b359705445f3985df5c201c479cc9b
Ryan-Millard <millardryandevon@gmail.com>
2026-07-09 12:38:10 +0200

    docs: final styles

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -30,6 +30,16 @@ const algoliaHeadTag = {
   content: "DB4B5FEC1545D32B",
 };
 
+const GitHubNavbarLink = `
+  <a href="https://github.com/Ryan-Millard/Img2Num" class="navbar__item navbar__link navbar__link--container" target="_blank" rel="noopener noreferrer" aria-label="GitHub repository">
+    <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor"> <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/> </svg>
+    GitHub
+  </a>
+`;
+const SponsorLink = `<a href="https://github.com/sponsors/Ryan-Millard" class="navbar__item navbar__link navbar__link--container" target="_blank" rel="noopener noreferrer" aria-label="Sponsor this project">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
+</a>`;
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Img2Num",
@@ -211,19 +221,19 @@ const config = {
           { to: "/blog", label: "Blog", position: "left" },
           { to: "/changelog", label: "Changelog", position: "left" },
           {
-            href: "https://github.com/Ryan-Millard/Img2Num",
-            label: "GitHub",
+            type: "html",
             position: "right",
+            value: GitHubNavbarLink,
+          },
+          {
+            type: "html",
+            position: "right",
+            value: SponsorLink,
           },
         ],
         hideOnScroll: false,
       },
       footer: {
-        logo: {
-          alt: "Img2Num Logo",
-          src: "img/favicon.svg",
-          height: 150,
-        },
         links: [
           {
             title: "Documentation",
@@ -276,6 +286,18 @@ const config = {
             ],
           },
           {
+            title: `Copyright © ${new Date().getFullYear()} Img2Num.`,
+              items: [
+                {
+                  html: `
+                    <a href="/">
+                      <img src="/img/favicon.svg" alt="Img2Num Logo" height="150" />
+                    </a>
+                  `,
+                },
+              ],
+          },
+          {
             title: "Download",
             items: [
               {
@@ -318,7 +340,6 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Img2Num.`,
       },
       tableOfContents: {
         minHeadingLevel: 2,
